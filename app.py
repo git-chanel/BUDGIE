47e5368b8c73cdb8b074fb1f1046e6e8d96caa21
kobitoko <6801424+kobitoko@users.noreply.github.com>
2024-03-04 21:56:26 -0700

    progress on ui

---
+++ app.py
@@ -0,0 +1,8 @@
+import sys
+from PyQt5.QtWidgets import ( QApplication, QDialog, QMainWindow, QMessageBox)
+from PyQt5.uic import loadUi
+
+# inside ui folder is the ui_main_window.py file, import the Ui_MainWindow class from that generated file.
+from ui.ui_main_window import Ui_MainWindow
+
+# TODO: https://realpython.com/qt-designer-python/#putting-everything-together-in-an-application
---
+++ ui/README.md
@@ -0,0 +1,7 @@
+
+The .ui files are made with QtDesigner. pyqt5 should've been installed through the requirements.txt  
+When in this directory do the following to generate a new `ui_main_window.py` file.
+
+```
+pyuic5 -o .\ui_main_window.py .\main_window.ui
+```
